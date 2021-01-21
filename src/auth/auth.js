f9e0f95e0b0c6476ac3d75196b62c1da3e0c160f
divyanat2009 <69719463+divyanat2009@users.noreply.github.com>
2021-01-21 16:40:57 -0600

    user authentication updates

---
+++ src/auth/auth.js
@@ -0,0 +1,9 @@
+var jwt = require('jsonwebtoken');
+const {jwtSecret} = require("../config");
+
+function verifyJWTToken(jwtToken)
+{
+    return jwt.verify(jwtToken, jwtSecret);
+}
+
+module.exports = verifyJWTToken; 
