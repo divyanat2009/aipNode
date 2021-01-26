a7e5eaf21a2919ccc009a579026f3df7b3d43c34
divyanat2009 <69719463+divyanat2009@users.noreply.github.com>
2021-01-25 21:52:05 -0600

    added semicolon

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const logger = require('./logger')
-const { NODE_ENV } = require('./config')
-const usersRouter = require('./users/users-router.js')
-const postsRouter = require('./posts/posts-router.js')
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const logger = require('./logger');
+const { NODE_ENV } = require('./config');
+const usersRouter = require('./users/users-router.js');
+const postsRouter = require('./posts/posts-router.js');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDoc = require("../swagger.json");
 const app = express()
@@ -15,9 +15,9 @@ const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
-app.use(morgan(morganOption))
-app.use(helmet())
-app.use(cors())
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
 
 //validate API_Token
 /*app.use(function validateBearerToken(req, res, next){
@@ -30,8 +30,8 @@ app.use(cors())
     next()
 })*/
 
-app.use('/api/users',usersRouter)
-app.use('/api/posts',postsRouter)
+app.use('/api/users',usersRouter);
+app.use('/api/posts',postsRouter);
 app.use('/api/doc', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.get('/',(req,res)=>{
@@ -39,15 +39,15 @@ app.get('/',(req,res)=>{
 })
 
 app.use(function errorHandler(error, req, res, next){
-    let response
+    let response;
     if(NODE_ENV === 'production'){
         response = {error :{message:'server error'}}
     }
     else{
-        console.error(error)
+        console.error(error);
         response = { message: error.message, error}
     }
     res.status(500).json(response)
 })
 
-module.exports = app
+module.exports = app;
